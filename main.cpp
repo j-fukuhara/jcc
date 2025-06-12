29f1ef878cd84f5388c4062964d56ee476176a51
j-fukuhara <junji.fukuhara.96@gmail.com>
2025-06-12 21:42:17 +0900

    implement subtraction

--- main.cpp
+++ main.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-#include <cstdlib>
+#include <string>
 
 int main(int argc, char* argv[]){
+  std::string input = argv[1];
+  auto iter = input.begin();
+  
   std::cout << ".intel_syntax noprefix" << std::endl
 	    << ".global main" << std::endl
-	    << "main:" << std::endl
-	    << "  mov rax, " << std::atoi(argv[1]) << std::endl
-	    << "  add rax, " << std::atoi(argv[2]) << std::endl
-	    << "  ret" << std::endl;
+	    << "main:" << std::endl;
+  std::cout << "  mov rax, " << *iter << std::endl;
+  while(++iter != input.end()){
+    if(*iter == '+'){
+      std::cout << "  add rax, " << *(++iter) << std::endl;
+    } else if(*iter == '-'){
+      std::cout << "  sub rax, " << *(++iter) << std::endl;
+    }
+  }
+  std::cout << "  ret" << std::endl;
   return 0;
 }
